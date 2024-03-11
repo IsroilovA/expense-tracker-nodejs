73e2c7d4b1d2ceb3aa2b698ac196d0c3debcb938
Alisher <aisroilov1029@gmail.com>
2024-03-11 12:33:58 +0500

    bug with redirecting to home page fixed

--- controllers/web/home/index.js
+++ controllers/web/home/index.js
@@ -7,7 +7,7 @@ const home_controller = {
         res.render('home');
     },
     add: async (req, res) =>{
-        res.render('home/add_update');
+        res.render('home/add_update', { mode: 'Add' });
     },
     update: async (req, res) =>{
         const eventData = await expense_service.getById(req.params.id);
